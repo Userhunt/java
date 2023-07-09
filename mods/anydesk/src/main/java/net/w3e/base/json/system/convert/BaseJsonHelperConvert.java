95b4e69fa2835d62bfcae05183b6f471af195cf8
Userhunt <dimburov@mail.ru>
2023-07-09 21:48:38 +0300

    clear core

--- mods/anydesk/src/main/java/net/w3e/base/json/system/convert/BaseJsonHelperConvert.java
+++
@@ -1,3 +0,0 @@
-package net.w3e.base.json.system.convert;
-
-public interface BaseJsonHelperConvert extends BaseJsonHelperConvertPrimitive, BaseJsonHelperConvertJson {}
--- mods/split_screen/src/main/java/net/w3e/base/json/system/read/BaseJsonHelperRead.java
+++
@@ -1,3 +0,0 @@
-package net.w3e.base.json.system.read;
-
-public interface BaseJsonHelperRead extends BaseJsonHelperReadPrimitive, BaseJsonHelperReadJson, BaseJsonHelperReadPrimitiveArray, BaseJsonHelperReadCollection {}
