8770feecbf1658a2a8ee4361aab8db5279b8d604
Userhunt <36658653+Userhunt@users.noreply.github.com>
2024-06-01 19:07:19 +0300

    Port End

--- src/java/net/w3e/base/collection/MapT.java
+++ src/java/net/w3e/base/collection/MapT.java
@@ -2,6 +2,7 @@ package net.w3e.base.collection;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class MapT<K> extends HashMap<K, Object> {
 
@@ -53,6 +54,54 @@ public class MapT<K> extends HashMap<K, Object> {
 	public final Number getNumber(K key) {
 		Number number = getT(key, Number.class);
 		return number == null ? 0 : number;
+	}
+
+	public final String getSting(K key) {
+		return getT(key);
+	}
+
+	public final <T extends Enum<T>> T getEnum(K key) {
+		return getT(key);
+	}
+
+	public final <T extends Enum<T>> T getEnum(K key, Class<T> clazz) {
+		return getEnum(key);
+	}
+
+	public final Boolean getBOOLEAN(K key) {
+		return getT(key);
+	}
+
+	public final Number getNUMBER(K key) {
+		return getT(key);
+	}
+
+	public final Byte getBYTE(K key) {
+		return getT(key);
+	}
+
+	public final Short getSHORT(K key) {
+		return getT(key);
+	}
+
+	public final Integer getINTEGER(K key) {
+		return getT(key);
+	}
+
+	public final Long getLONG(K key) {
+		return getT(key);
+	}
+
+	public final Float getFLOAT(K key) {
+		return getT(key);
+	}
+
+	public final Double getDOUBLE(K key) {
+		return getT(key);
+	}
+
+	public final UUID getUUID(K key) {
+		return getT(key);
 	}
 
 	public static class MapTString extends MapT<String> {
